05977e6a33ff8fea21ed46de4f4fb98f8041ea5d
emaCtrader <87609536+emaCtrader@users.noreply.github.com>
2026-03-11 20:12:41 +0100

    implementing read_text method

--- FastAPI/Intelligence_ocr_llm/services/ocr_reader.py
+++ FastAPI/Intelligence_ocr_llm/services/ocr_reader.py
@@ -4,13 +4,34 @@ import pytesseract
 
 from typing import List
 
-
-
-
 class OCRReader:
 
     def __init__(self):
 
-        self.modello = pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+        pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-    async def read_text(self, image_path: str) -> List[dict]:
+        self.configurazione_tesseract = r'--oem 3 --psm 6'
+
+
+    async def read_text(self, image, layout_yolo: List[dict]) -> List[dict]:
+
+       result = []
+
+       for element in layout_yolo:
+          
+           x1, y1, x2, y2 = element['x1'],element['y1'],element['x2'],element['y2']
+
+           image_cropped = image[y1:y2, x1:x2]
+       
+           ritaglio_grigio = cv2.cvtColor(image_cropped, cv2.COLOR_BGR2RGB)
+
+           testo_letto = pytesseract.image_to_string(
+                ritaglio_grigio, 
+                config=self.configurazione_tesseract
+            )
+
+           testo_letto_ = testo_letto.strip()
+
+           result.append(testo_letto_)
+
+       return result
